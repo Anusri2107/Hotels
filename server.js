18b5fccb4502c1fcd42e0093dd3e52407ebf0e0f
Anusri2107 <anusrimodi21@gmail.com>
2024-05-08 15:59:38 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to our Hotel')
@@ -33,6 +35,6 @@ app.use('/person',personRoutes);
 const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('Listening on port 3000');
 })
